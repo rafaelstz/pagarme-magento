2cc668af0e30b8d29e9fec426542fe4383c1d318
Michel Brito <micheljunio@gmail.com>
2014-08-22 20:03:20 -0300

    Adicionado compatibilidade com o One Step Checkout Brasil

--- skin/frontend/base/default/pagarme/js/checkout.js
+++ skin/frontend/base/default/pagarme/js/checkout.js
@@ -5,41 +5,69 @@
  * @author     Suporte <suporte@inovarti.com.br>
  */
 
-Payment.prototype._save = Payment.prototype.save;
-Payment.prototype.save = function() {
-    if (checkout.loadWaiting!=false) return;
-    var validator = new Validation(this.form);
-    if (this.validate() && validator.validate()) {
-        if (this.currentMethod == 'pagarme_cc') {
-            this.pagarme_cc_data = {};
-            var fields = ['installments', 'cc_type', 'cc_number', 'cc_owner', 'expiration', 'expiration_yr', 'cc_cid'];
-            fields.each(function(field){
-                this.pagarme_cc_data[field] = $(this.currentMethod+'_'+field).value;
+if (typeof OPC !== "undefined") {
+    // One Step Checkout Brasil
+    OPC.prototype._save = OPC.prototype.save;
+    OPC.prototype.save = function() {
+        if (checkout.loadWaiting!=false) return;
+        if (this.validator.validate()) {
+            if (payment.currentMethod == 'pagarme_cc') {
+                var creditCard = new PagarMe.creditCard();
+                creditCard.cardHolderName = $(payment.currentMethod+'_cc_owner').value;
+                creditCard.cardExpirationMonth = $(payment.currentMethod+'_expiration').value;
+                creditCard.cardExpirationYear = $(payment.currentMethod+'_expiration_yr').value;
+                creditCard.cardNumber = $(payment.currentMethod+'_cc_number').value;
+                creditCard.cardCVV = $(payment.currentMethod+'_cc_cid').value;
+
+                $('review-please-wait').show();
+                creditCard.generateHash(function(cardHash) {
+                    $('review-please-wait').hide();
+                    $(payment.currentMethod+'_pagarme_card_hash').value = cardHash;
+                    this._save();
+                }.bind(this));
+            } else {
+                this._save();
+            }
+        }
+    }
+} else {
+    // Default Magento Checkout
+    Payment.prototype._save = Payment.prototype.save;
+    Payment.prototype.save = function() {
+        if (checkout.loadWaiting!=false) return;
+        var validator = new Validation(this.form);
+        if (this.validate() && validator.validate()) {
+            if (this.currentMethod == 'pagarme_cc') {
+                this.pagarme_cc_data = {};
+                var fields = ['installments', 'cc_type', 'cc_number', 'cc_owner', 'expiration', 'expiration_yr', 'cc_cid'];
+                fields.each(function(field){
+                    this.pagarme_cc_data[field] = $(this.currentMethod+'_'+field).value;
+                }.bind(this));
+            } else {
+                this.pagarme_cc_data = null; //clear data
+            }
+            this._save();
+        }
+    };
+
+    Review.prototype._save = Review.prototype.save;
+    Review.prototype.save = function() {
+        if (payment.currentMethod == 'pagarme_cc') {
+            var creditCard = new PagarMe.creditCard();
+            creditCard.cardHolderName = $(payment.currentMethod+'_cc_owner').value;
+            creditCard.cardExpirationMonth = $(payment.currentMethod+'_expiration').value;
+            creditCard.cardExpirationYear = $(payment.currentMethod+'_expiration_yr').value;
+            creditCard.cardNumber = $(payment.currentMethod+'_cc_number').value;
+            creditCard.cardCVV = $(payment.currentMethod+'_cc_cid').value;
+
+            checkout.setLoadWaiting('review');
+            creditCard.generateHash(function(cardHash) {
+                checkout.setLoadWaiting(false);
+                $(payment.currentMethod+'_pagarme_card_hash').value = cardHash;
+                this._save();
             }.bind(this));
         } else {
-            this.pagarme_cc_data = null; //clear data
-        }
-        this._save();
-    }
-};
-
-Review.prototype._save = Review.prototype.save;
-Review.prototype.save = function() {
-    if (payment.currentMethod == 'pagarme_cc') {
-        var creditCard = new PagarMe.creditCard();
-        creditCard.cardHolderName = payment.pagarme_cc_data.cc_owner;
-        creditCard.cardExpirationMonth = payment.pagarme_cc_data.expiration;
-        creditCard.cardExpirationYear = payment.pagarme_cc_data.expiration_yr;
-        creditCard.cardNumber = payment.pagarme_cc_data.cc_number;
-        creditCard.cardCVV = payment.pagarme_cc_data.cc_cid;
-
-        checkout.setLoadWaiting('review');
-        creditCard.generateHash(function(cardHash) {
-            checkout.setLoadWaiting(false);
-            $(payment.currentMethod+'_pagarme_card_hash').value = cardHash;
             this._save();
-        }.bind(this));
-    } else {
-        this._save();
+        }
     }
 }
