064ae282c57abdb47f5ed47ac657c7ab9b2460b9
Eneias Ramos de Melo <eneias@gamuza.com.br>
2015-07-03 02:17:56 -0300

    Adicionado loader para carregamento JS externo

--- skin/frontend/base/default/pagarme/js/checkout.js
+++ skin/frontend/base/default/pagarme/js/checkout.js
@@ -5,6 +5,30 @@
  * @author     Suporte <suporte@inovarti.com.br>
  */
 
+function pagarmeDocumentHeight()
+{
+    var D = document;
+
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
+
+function pagarmeShowLoader ()
+{
+    $("pagarme-overlay").setStyle({ height: pagarmeDocumentHeight() + "px" });
+    $("pagarme-overlay").show ();
+    $("pagarme-mask").show ();
+}
+
+function pagarmeHideLoader ()
+{
+    $("pagarme-mask").hide ();
+    $("pagarme-overlay").hide ();
+}
+
 function pagarmeDisableAll(element){
     $$(element).each(function(obj){
         $(obj).disable();
@@ -12,7 +36,40 @@ function pagarmeDisableAll(element){
     });
 }
 
-document.observe("dom:loaded",function(){
+function pagarmeInitCheckout()
+{
+
+PagarMe.encryption_key = pagarme_encryption_key;
+
+PagarMe._ajax = PagarMe.ajax;
+PagarMe.ajax = function (url, callback) {
+    var httpRequest,
+        xmlDoc;
+
+    if (window.XMLHttpRequest) {
+        httpRequest = new XMLHttpRequest();
+    } else {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    pagarmeShowLoader ();
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState != 4) {
+            return;
+        }
+
+        if (httpRequest.status != 200 && httpRequest.status != 304) {
+            return;
+        }
+        callback(JSON.parse(httpRequest.responseText));
+
+        pagarmeHideLoader ();
+    };
+
+    httpRequest.open("GET", url, true);
+    httpRequest.send(null);
+};
 
 if (typeof OSCPayment !== "undefined") {
     // One Step Checkout Brasil 6 Pro
@@ -166,6 +223,27 @@ if (typeof OSCPayment !== "undefined") {
         }
     }
 }
+
+} // pagarmeInitCheckout
+
+document.observe("dom:loaded",function(){
+
+pagarmeShowLoader ();
+
+var pagarmeJS = document.createElement('script');
+pagarmeJS.type = "text/javascript";
+pagarmeJS.async = true;
+pagarmeJS.src = 'https://pagar.me/assets/pagarme.js';
+pagarmeJS.addEventListener('load', function(){
+    pagarmeInitCheckout ();
+
+    console.log('Pagarme: Ready');
+
+    pagarmeHideLoader ();
+}, false);
+
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(pagarmeJS);
 
 });
 
