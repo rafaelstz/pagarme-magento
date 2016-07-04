bc7a233464173ba3cdd12f3b7ff0647cfd052e42
socialgroups <socialgroups@outlook.com>
2016-07-04 18:39:44 -0300

    Fix - Warning: Missing argument 1 for Mage_Core_Model_Resource_Setup

--- app/code/community/Inovarti/Pagarme/sql/pagarme_setup/upgrade-0.1.3-0.1.4.php
+++ app/code/community/Inovarti/Pagarme/sql/pagarme_setup/upgrade-0.1.3-0.1.4.php
@@ -1,6 +1,6 @@
 <?php
 
-$installer = new Mage_Sales_Model_Resource_Setup();
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
 $installer->startSetup();
 
 $entities = array(
@@ -8,14 +8,12 @@ $entities = array(
     'order_item'
 );
 
-$options = array(
-    'type'     => Varien_Db_Ddl_Table::TYPE_VARCHAR,
-    'visible'  => true,
-    'required' => false
-);
-
 foreach ($entities as $entity) {
-    $installer->addAttribute($entity, 'recipient_id', $options);
+    $installer->addAttribute($entity, 'recipient_id', array(
+        'type'     => Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        'visible'  => true,
+        'required' => false
+    ));
 }
 
 $installer->endSetup();
