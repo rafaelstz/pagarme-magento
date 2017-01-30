6a952a5e6b46219274df9d8340f211f28c49707a
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-01-30 16:52:12 -0200

    Fix of total in creditmemo

--- app/code/community/Inovarti/Pagarme/Model/Sales/Order/Total/Creditmemo/Fee.php
+++ app/code/community/Inovarti/Pagarme/Model/Sales/Order/Total/Creditmemo/Fee.php
@@ -1,31 +1,20 @@
 <?php
+
 /*
  * @copyright   Copyright (C) 2015 Gamuza Technologies (http://www.gamuza.com.br/)
  * @author     Eneias Ramos de Melo <eneias@gamuza.com.br>
  */
-
 class Inovarti_Pagarme_Model_Sales_Order_Total_Creditmemo_Fee extends Mage_Sales_Model_Order_Creditmemo_Total_Abstract
 {
     public function collect(Mage_Sales_Model_Order_Creditmemo $creditmemo)
     {
         $order = $creditmemo->getOrder();
-        if ($order->getFeeAmountInvoiced() > 0) {
-            $feeAmountLeft = $order->getFeeAmountInvoiced() - $order->getFeeAmountRefunded();
-            $basefeeAmountLeft = $order->getBaseFeeAmountInvoiced() - $order->getBaseFeeAmountRefunded();
-            if ($basefeeAmountLeft > 0) {
-                $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $feeAmountLeft);
-                $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $basefeeAmountLeft);
-                $creditmemo->setFeeAmount($feeAmountLeft);
-                $creditmemo->setBaseFeeAmount($basefeeAmountLeft);
-            }
-        } else {
-            $feeAmount = $order->getFeeAmountInvoiced();
-            $basefeeAmount = $order->getBaseFeeAmountInvoiced();
-            $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $feeAmount);
-            $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $basefeeAmount);
-            $creditmemo->setFeeAmount($feeAmount);
-            $creditmemo->setBaseFeeAmount($basefeeAmount);
-        }
+        $feeAmount = $order->getFeeAmount();
+        $baseFeeAmount = $order->getBaseFeeAmount();
+        $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $feeAmount);
+        $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $baseFeeAmount);
+        $creditmemo->setFeeAmount($feeAmount);
+        $creditmemo->setBaseFeeAmount($baseFeeAmount);
 
         return $this;
     }
