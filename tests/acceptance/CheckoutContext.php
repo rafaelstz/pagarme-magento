7112ab7ae9b7c6d3517f92af2c6f2f7334cb9c5c
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-02-14 14:46:25 -0200

    Refact e2e checkout test

--- tests/acceptance/CheckoutContext.php
+++ tests/acceptance/CheckoutContext.php
@@ -13,6 +13,22 @@ class CheckoutContext extends MinkContext
     private $session;
 
     /**
+     * @BeforeScenario
+     */
+    public function setUp() 
+    {
+        $this->magentoUrl = getenv('MAGENTO_URL');
+        $this->session = $this->getSession();
+    }
+
+    public function waitForElement($element, $timeout) {
+        $this->session->wait(
+            $timeout,
+            "document.querySelector('${element}').style.display != 'none'"
+        );
+    }
+
+    /**
      * @Given a registered user
      */
     public function aRegisteredUser()
@@ -21,17 +37,16 @@ class CheckoutContext extends MinkContext
     }
 
     /**
-     * @Given any product
-     */
-    public function anyProduct()
-    {
-    }
-
-    /**
      * @Given a valid credit card
      */
     public function aValidCreditCard()
     {
+        $this->creditCard = [
+            'customer_name'   => $this->customer->getName(),
+            'number'          => '4111111111111111',
+            'cvv'             => '123',
+            'expiration_date' => '0220'
+        ];
     }
 
     /**
@@ -39,8 +54,8 @@ class CheckoutContext extends MinkContext
      */
     public function iAccessTheStorePage()
     {
-        $this->getSession()
-            ->visit(getenv('MAGENTO_URL'));
+        $this->session
+            ->visit($this->magentoUrl);
     }
 
     /**
@@ -48,10 +63,11 @@ class CheckoutContext extends MinkContext
      */
     public function addAnyProductToBasket()
     {
-        $page = $this->getSession()
-            ->getPage();
+        $page = $this->session->getPage();
 
-        $page->pressButton('Add to Cart');
+        $page->pressButton(
+            Mage::helper('pagarme_checkout')->__('Add to Cart')
+        );
     }
 
     /**
@@ -59,10 +75,11 @@ class CheckoutContext extends MinkContext
      */
     public function iGoToCheckoutPage()
     {
-        $page = $this->getSession()
-            ->getPage();
+        $page = $this->session->getPage();
 
-        $page->pressButton('Proceed to Checkout');
+        $page->pressButton(
+            Mage::helper('pagarme_checkout')->__('Proceed to Checkout')
+        );
     }
 
     /**
@@ -70,70 +87,40 @@ class CheckoutContext extends MinkContext
      */
     public function loginWithRegisteredUser()
     {
-        $page = $this->getSession()
-            ->getPage();
+        $page = $this->session->getPage();
 
-        $inputEmail = $page->find('named', array('id', 'login-email'));
-        $inputEmail->setValue($this->customer->getEmail());
-
-        $inputPassword = $page->find('named', array('id', 'login-password'));
-        $inputPassword->setValue($this->customer->getPassword());
+        $this->fillField(
+            Mage::helper('pagarme_checkout')->__('Email Address'), 
+            $this->customer->getEmail()
+        );
+        
+        $this->fillField(
+            Mage::helper('pagarme_checkout')->__('Password'), 
+            $this->customer->getPassword()
+        );
 
         $page->pressButton('Login');
     }
 
     /**
-     * @When confirm billing information
+     * @When confirm billing and shipping address information
      */
-    public function confirmBillingInformation()
+    public function confirmBillingAndShippingAddressInformation()
     {
-        $session = $this->getSession();
-        $page = $session->getPage();
+        $page = $this->session->getPage();
         $page->find('css', '#billing-buttons-container button')->press();
-    }
 
-    /**
-     * @When select shipping method
-     */
-    public function selectShippingMethod()
-    {
-        $session = $this->getSession();
-        $session->wait(
-            5000,
-            "document.querySelector('#checkout-step-shipping_method').style.display != 'none'"
+        $this->waitForElement('#checkout-step-shipping_method', 5000);
+
+        $page->find('css', '#shipping-method-buttons-container button')
+            ->press();
+
+        $this->waitForElement('#checkout-step-payment', 5000);
+
+        $page->find('css', '#p_method_pagarme_checkout')->click();
+        $page->pressButton(
+            Mage::helper('pagarme_checkout')->__('Fill in the card data')
         );
-        $page = $session->getPage();
-        $page->find('css', '#shipping-method-buttons-container button')->press();
-    }
-
-    /**
-     * @When select payment method
-     */
-    public function selectPaymentMethod()
-    {
-        $session = $this->getSession();
-
-        $session->wait(
-            5000,
-            "document.querySelector('#checkout-step-payment').style.display != 'none'"
-        );
-
-        $page = $session->getPage();
-        $page->find(
-            'named',
-            array(
-                'id',
-                'p_method_pagarme_checkout'
-            )
-        )->click();
-
-        $page->pressButton(Mage::helper('pagarme_checkout')->__('Fill in the card data'));
-
-        $session->wait(
-            1000,
-            "document.querySelector('#pagarme-checkout-ui') != null"
-        );    
-
     }
 
     /**
@@ -141,131 +128,131 @@ class CheckoutContext extends MinkContext
      */
     public function iUseAValidCreditCardToPay()
     {
-        $session = $this->getSession();
+        $page = $this->session->getPage();
 
-        $page = $session->getPage();
+        $this->session->switchToIframe(
+            $page->find('css' ,'iframe')->getAttribute('name')
+        );
 
-        $session->switchToIframe($page->find('css' ,'iframe')->getAttribute('name'));
-
-        $pagarMeCheckout = $session->getPage();
+        $pagarMeCheckout = $this->session->getPage();
         
         $pagarMeCheckout->pressButton('Cartão de crédito');
 
-        $session->wait(
-            1000,
-            "document.querySelector('#pagarme-modal-box-step-buyer-information').style.display != 'none'"
+        $this->waitForElement(
+            '#pagarme-modal-box-step-buyer-information',
+            1000
         );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-buyer-name'
-        )->setValue($this->customer->getName());
+        $this->fillField(
+            'pagarme-modal-box-buyer-name',
+            $this->customer->getName()
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-buyer-email'
-        )->setValue($this->customer->getEmail());
+        $this->fillField(
+            'pagarme-modal-box-buyer-email', 
+            $this->customer->getEmail()
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-buyer-document-number'
-        )->setValue($this->customer->getTaxvat());
+        $this->fillField(
+            'pagarme-modal-box-buyer-document-number',
+            $this->customer->getTaxvat()
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-buyer-ddd'
-        )->setValue('11');
+        $this->fillField(
+            'pagarme-modal-box-buyer-ddd', 
+            '11'
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-buyer-number'
-        )->setValue('995551668');
+        $this->fillField(
+            'pagarme-modal-box-buyer-number', 
+            '995551668'
+        );
 
         $pagarMeCheckout->find(
             'css', 
             '#pagarme-modal-box-step-buyer-information .pagarme-modal-box-next-step'
         )->click();
 
-        $session->wait(
-            1000,
-            "document.querySelector('#pagarme-modal-box-step-customer-address-information').style.display != 'none'"
+        $this->waitForElement(
+            '#pagarme-modal-box-step-customer-address-information',
+            1000
         );
 
         $billingAddress = FeatureContext::getCustomerAddress();
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-zipcode'
-        )->setValue($billingAddress->getPostcode());
+        $this->fillField(
+            'pagarme-modal-box-customer-address-zipcode',
+            $billingAddress->getPostcode()
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-street'
-        )->setValue($billingAddress->getStreet()[0]);
+        $this->fillField(
+            'pagarme-modal-box-customer-address-street',
+            $billingAddress->getStreet()[0]
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-number'
-        )->setValue($billingAddress->getStreet()[1]);
+        $this->fillField(
+            'pagarme-modal-box-customer-address-number',
+            $billingAddress->getStreet()[1]
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-complementary'
-        )->setValue($billingAddress->getStreet()[2]);
+        $this->fillField(
+            'pagarme-modal-box-customer-address-complementary',
+            $billingAddress->getStreet()[2]
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-neighborhood'
-        )->setValue($billingAddress->getStreet()[3]);
+        $this->fillField(
+            'pagarme-modal-box-customer-address-neighborhood',
+            $billingAddress->getStreet()[3]
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-city'
-        )->setValue($billingAddress->getCity());
+        $this->fillField(
+            'pagarme-modal-box-customer-address-city',
+            $billingAddress->getCity()
+        );
 
-        $pagarMeCheckout->find(
-            'css', 
-            '#pagarme-modal-box-customer-address-state'
-        )->setValue($billingAddress->getState());
+        $this->fillField(
+            'pagarme-modal-box-customer-address-state',
+            $billingAddress->getState()
+        );
 
         $pagarMeCheckout->find(
             'css', 
             '#pagarme-modal-box-step-customer-address-information .pagarme-modal-box-next-step'
         )->click();
 
-        $session->wait(
-            1000,
-            "document.querySelector('#pagarme-modal-box-step-credit-card-information').style.display != 'none'"
+        $this->waitForElement(
+            '#pagarme-modal-box-step-credit-card-information',
+            1000
         );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-credit-card-number'
-        )->setValue('4242424242424242');
+        $this->fillField(
+            'pagarme-modal-box-credit-card-number',
+            $this->creditCard['number']
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-credit-card-name'
-        )->setValue($this->customer->getName());
+        $pagarMeCheckout->fillField(
+            'pagarme-modal-box-credit-card-name',
+            $this->creditCard['customer_name']
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-credit-card-expiration'
-        )->setValue('1020');
+        $pagarMeCheckout->fillField(
+            'pagarme-modal-box-credit-card-expiration',
+            $this->creditCard['expiration_date']
+        );
 
-        $pagarMeCheckout->find(
-            'css',
-            '#pagarme-modal-box-credit-card-cvv'
-        )->setValue('123');
+        $pagarMeCheckout->fillField(
+            'pagarme-modal-box-credit-card-cvv',
+            $this->creditCard['cvv']
+        );
 
         $pagarMeCheckout->find(
             'css', 
             '#pagarme-modal-box-step-credit-card-information .pagarme-modal-box-next-step'
         )->click();
 
-        $session->switchToIframe();
+        $this->session->switchToIframe();
 
-        $session->wait(
+        $this->session->wait(
             5000,
             "document.querySelector('#pagarme-checkout-container').style.display == 'none'"
         );
@@ -275,10 +262,7 @@ class CheckoutContext extends MinkContext
             '#payment-buttons-container button'
         )->press();
 
-        $session->wait(
-            2000,
-            "document.querySelector('#checkout-step-review').style.display != 'none'"
-        );
+        $this->waitForElement('#checkout-step-review', 2000);
 
         $page->pressButton(Mage::helper('pagarme_checkout')->__('Place Order'));
     }
@@ -288,15 +272,23 @@ class CheckoutContext extends MinkContext
      */
     public function thePurchaseMustBePaidWithSuccess()
     {
-        $session = $this->getSession();
-        $session->wait(5000);
+        $this->session->wait(5000);
 
-        $page = $session->getPage();
+        $page = $this->session->getPage();
 
         $successMsg = $page->find('css', 'h1')
             ->getText();
 
-        \PHPUnit_Framework_TestCase::assertEquals(getenv('MAGENTO_URL') . 'index.php/checkout/onepage/success/', $session->getCurrentUrl());
-        \PHPUnit_Framework_TestCase::assertEquals(Mage::helper('pagarme_checkout')->__('Your order has been received.'), $successMsg);
+        \PHPUnit_Framework_TestCase::assertEquals(
+            getenv('MAGENTO_URL') . 'index.php/checkout/onepage/success/', 
+            $this->session->getCurrentUrl()
+        );
+
+        \PHPUnit_Framework_TestCase::assertEquals(
+            Mage::helper(
+                'pagarme_checkout')->__('Your order has been received.'
+            ), 
+            $successMsg
+        );
     }
 }
--- tests/acceptance/features/checkout.feature
+++ tests/acceptance/features/checkout.feature
@@ -5,15 +5,12 @@ Feature: Checkout Pagar.me
 
     Scenario: Make a purchase by credit card
         Given a registered user
-        And any product
         And a valid credit card
         When I access the store page
         And add any product to basket
         And I go to checkout page
         And login with registered user
-        And confirm billing information
-        And select shipping method
-        And select payment method
+        And confirm billing and shipping address information
         And I use a valid credit card to pay
         Then the purchase must be paid with success
 
