c83a93796bb9e536eae2e2d9abb82160031df921
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2017-02-24 09:15:40 -0300

    Validate request from postback

--- app/code/community/PagarMe/Core/controllers/Transaction/BoletoController.php
+++ app/code/community/PagarMe/Core/controllers/Transaction/BoletoController.php
@@ -7,8 +7,11 @@ class PagarMe_Core_Transaction_BoletoController extends Mage_Core_Controller_Fro
         $request = $this->getRequest();
 
         if (!$request->isPost()) {
-            return $this->getResponse()
-                ->setHttpResponseCode(405);
+            return $this->getResponse()->setHttpResponseCode(405);
+        }
+
+        if (!$this->isValidRequest($request)) {
+            return $this->getResponse()->setHttpResponseCode(400);
         }
 
         $transactionId = $request->getPost('id');
@@ -27,5 +30,37 @@ class PagarMe_Core_Transaction_BoletoController extends Mage_Core_Controller_Fro
                 ->setHttpResponseCode(500)
                 ->setBody($e->getMessage());
         }
+    }
+
+    private function isValidRequest(Mage_Core_Controller_Request_Http $request)
+    {
+        if ($request->getPost('id') == null) {
+            return false;
+        }
+
+        if ($request->getPost('current_status') == null) {
+            return false;
+        }
+
+        $signature = $request->getHeader('X-Hub-Signature');
+
+        if ($signature == false) {
+            return false;
+        }
+
+        if (!$this->isAuthenticRequest($request, $signature)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isAuthenticRequest(
+        Mage_Core_Controller_Request_Http $request,
+        $signature
+    ) {
+        return Mage::getModel('pagarme_core/sdk_adapter')->getPagarMeSdk()
+            ->postback()
+            ->validateRequest($request->getRawBody(), $signature);
     }
 }
