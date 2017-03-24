29fae04b411a40fedc18bd9d48644402cadfdbe9
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-03-24 13:06:04 -0300

    Fix how to load api key

--- app/code/community/PagarMe/Core/Model/Postback/Boleto.php
+++ app/code/community/PagarMe/Core/Model/Postback/Boleto.php
@@ -2,10 +2,21 @@
 
 class PagarMe_Core_Model_Postback_Boleto extends Mage_Core_Model_Abstract
 {
+    const POSTBACK_STATUS_PAID = 'paid';
+    const POSTBACK_STATUS_REFUNDED = 'refunded';
+
     /**
      * @var PagarMe_Core_Model_Service_Invoice
      */
     protected $invoiceService;
+
+    /**
+     * @var array
+     */
+    protected $validStatus = [
+        self::POSTBACK_STATUS_PAID,
+        self::POSTBACK_STATUS_REFUNDED
+    ];
 
     /**
      * @param Mage_Sales_Model_Order $order
@@ -15,7 +26,15 @@ class PagarMe_Core_Model_Postback_Boleto extends Mage_Core_Model_Abstract
      */
     private function canProceedWithPostback(Mage_Sales_Model_Order $order, $currentStatus)
     {
-        return $order->canInvoice() && $currentStatus == 'paid';
+        return $order->canInvoice() && $this->isValidStatus($currentStatus);
+    }
+
+    /**
+     * @param string $status
+     */
+    private function isValidStatus($status)
+    {
+        return in_array($status, $this->validStatus);
     }
 
     /**
@@ -80,6 +99,20 @@ class PagarMe_Core_Model_Postback_Boleto extends Mage_Core_Model_Abstract
             );
         }
 
+        switch ($currentStatus) {
+            case self::POSTBACK_STATUS_PAID:
+                $this->setOrderAsPaid($order);
+                break;
+            case self::POSTBACK_STATUS_REFUNDED:
+                $this->setOrderAsRefunded($order);
+                break;
+        }
+
+        return $order;
+    }
+
+    public function setOrderAsPaid($order)
+    {
         $invoice = $this->getInvoiceService()
             ->createInvoiceFromOrder($order);
 
@@ -92,7 +125,10 @@ class PagarMe_Core_Model_Postback_Boleto extends Mage_Core_Model_Abstract
             ->addObject($order)
             ->addObject($invoice)
             ->save();
+    }
 
-        return $order;
+    public function setOrderAsRefunded($order)
+    {
+        $order->setState(Mage_Sales_Model_Order::STATE_CLOSED);
     }
 }
--- tests/acceptance/features/postback.feature
+++ tests/acceptance/features/postback.feature
@@ -3,10 +3,14 @@ Feature: Update order status
     I want receive updates about transactions status
     To keep order workflow updated and financial health of my store
 
-    Scenario: Receiving boleto order status update
+    Scenario Outline: Receiving boleto order status update
         Given a pending boleto order
-        When I receive a postback for boleto with status "paid"
-        Then my order must be updated to "processing"
+        When I receive a postback for boleto with status "<postback_status>"
+        Then my order must be updated to "<new_status>"
+        Examples:
+        | postback_status | new_status |
+        | paid            | processing |
+        | refunded        | closed     |
 
     Scenario: Receiving credit card order status update
         Given a pending credit card order
