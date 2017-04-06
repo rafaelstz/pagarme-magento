167bd032ce58ccbf42b61962b23478d1ae023a5e
ricardotulio <ledo.tulio@gmail.com>
2017-04-06 18:06:22 -0300

    Checkout PagarMe: Create discount item Create abstract class to provide common methods to boleto and interest fee and create specific classes to boleto discount and interest fee amount

---
+++ app/code/community/PagarMe/Core/Model/Quote/Address/Total/CreditCardInterestAmount.php
@@ -0,0 +1,93 @@
+<?php
+
+class PagarMe_Checkout_Model_Total
+ extends PagarMe_Core_Model_Quote_Address_Total_Abstract
+{
+    private $interestAmount;
+
+    public function __construct()
+    {
+        $this->setCode('pagarme_checkout_credit_card');
+    }
+
+    /**
+     * Get label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return Mage::helper('pagarme_checkout')->__('Interest fee');
+    }
+
+    /**
+     * @param Mage_Sales_Model_Quote_Address $address
+     * @return PagarMe_Checkout_Model_Total
+     */
+    public function collect(Mage_Sales_Model_Quote_Address $address)
+    {
+        parent::collect($address);
+        
+        if (!$this->shouldCollect()) {
+            return $this;
+        }
+                
+                $quote = $address->getQuote();
+        $subtotalAmount = $this->getSubtotal($quote);
+        
+        $transaction = $this->getTransaction();
+        $totalAmount = Mage::helper('pagarme_core')
+            ->parseAmountToFloat($transaction->getAmount());
+        
+        $this->interestAmount = $totalAmount - $subtotalAmount;
+        
+        if ($this->interestAmount > 0) {
+            $this->_addAmount($this->interestAmount);
+            $this->_addBaseAmount($this->interestAmount);
+        }
+        
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function shouldCollect()
+    {
+        if (!parent::shouldCollect()) {
+            return false;
+        }
+
+        $transaction = $this->getTransaction();
+
+        if (!$transaction instanceof \PagarMe\Sdk\Transaction\CreditCardTransaction) {
+            return false;
+        }
+
+        if ($this->interestAmount > 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Add giftcard totals information to address object
+     *
+     * @param Mage_Sales_Model_Quote_Address $address
+     */
+    public function fetch(Mage_Sales_Model_Quote_Address $address)
+    {
+        $addressTotalAmount = $address->getTotalAmount($this->getCode());
+
+        if ($this->interestAmount != 0 && $addressTotalAmount == 0) {
+            $address->addTotal(array(
+                'code'  => $this->getCode(),
+                'title' => $this->getLabel(),
+                'value' => $this->interestAmount
+            ));
+        }
+
+        return $this;
+    }
+}
