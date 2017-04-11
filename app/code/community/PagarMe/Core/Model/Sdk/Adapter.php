44345e815b07c9d393e8240838b8e7630082f790
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2017-04-11 11:35:43 -0300

    Refact user agent method

--- app/code/community/PagarMe/Core/Model/Sdk/Adapter.php
+++ app/code/community/PagarMe/Core/Model/Sdk/Adapter.php
@@ -11,19 +11,10 @@ class PagarMe_Core_Model_Sdk_Adapter extends Mage_Core_Model_Abstract
     {
         parent::_construct();
 
-        $headers = [
-            'User-Agent' => sprintf(
-                'Magento/%s PagarMe/%s PHP/%s',
-                Mage::getVersion(),
-                Mage::getConfig()->getNode()->modules->PagarMe_Core->version,
-                phpversion()
-            )
-        ];
-
         $this->pagarMeSdk = new \PagarMe\Sdk\PagarMe(
             Mage::getStoreConfig('payment/pagarme_settings/api_key'),
             null,
-            $headers
+            $this->getUserAgent()
         );
     }
 
@@ -33,5 +24,20 @@ class PagarMe_Core_Model_Sdk_Adapter extends Mage_Core_Model_Abstract
     public function getPagarMeSdk()
     {
         return $this->pagarMeSdk;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUserAgent()
+    {
+        return [
+            'User-Agent' => sprintf(
+                'Magento/%s PagarMe/%s PHP/%s',
+                Mage::getVersion(),
+                Mage::getConfig()->getNode()->modules->PagarMe_Core->version,
+                phpversion()
+            )
+        ];
     }
 }
