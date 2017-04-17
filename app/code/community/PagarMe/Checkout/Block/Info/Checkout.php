9245cf07b370f6b41cb99194efb054f3ab550dd6
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2017-04-17 16:05:54 -0300

    Refactor
    Now the Order info is provided for both checkout/customer/admin scenarios, along with all the required fee information.

--- app/code/community/PagarMe/Checkout/Block/Info/Checkout.php
+++ app/code/community/PagarMe/Checkout/Block/Info/Checkout.php
@@ -2,21 +2,23 @@
 
 class PagarMe_Checkout_Block_Info_Checkout extends Mage_Payment_Block_Info
 {
+    /**
+     * @var \PagarMe\Sdk\Transaction\AbstractTransaction
+     */
     protected $transaction;
     
     const PAYMENT_METHOD_CREDIT_CARD_LABEL = 'Cartão de Crédito';
     const PAYMENT_METHOD_BOLETO_LABEL = 'Boleto';
 
-    /**
-     * @codeCoverageIgnore
-     *
-     * @return void
-     */
     public function _construct()
     {
         parent::_construct();
 
-        $this->setTemplate('pagarme/checkout/order_info/payment_details.phtml');
+        if (Mage::app()->getStore()->isAdmin()) {
+            $this->setTemplate(
+                'pagarme/checkout/order_info/payment_details.phtml'
+            );
+        }
     }
 
     /**
@@ -26,16 +28,31 @@ class PagarMe_Checkout_Block_Info_Checkout extends Mage_Payment_Block_Info
      */
     public function getTransaction()
     {
-        if (is_null($this->transaction)) {
-            $this->transaction =  \Mage::getModel('pagarme_core/service_order')
+        $order = $this->getInfo()->getOrder();
+
+        if (is_null($this->transaction) && !is_null($order)) {
+            $this->transaction = \Mage::getModel('pagarme_core/service_order')
                 ->getTransactionByOrderId(
-                    $this->getInfo()
-                        ->getOrder()
-                        ->getId()
+                    $order->getId()
                 );
+
+            return $this->transaction;
         }
 
-        return $this->transaction;
+        $additionalInformation = $this->getInfo()->getAdditionalInformation();
+
+        if (is_array($additionalInformation)
+            && isset($additionalInformation['token'])
+        ) {
+            $this->transaction = \Mage::getModel('pagarme_core/sdk_adapter')
+                ->getPagarMeSdk()
+                ->transaction()
+                ->get($additionalInformation['token']);
+
+            return $this->transaction;
+        }
+
+        throw new \Exception('Transaction was not found.');
     }
 
     /**
@@ -47,5 +64,29 @@ class PagarMe_Checkout_Block_Info_Checkout extends Mage_Payment_Block_Info
             == \PagarMe\Sdk\Transaction\CreditCardTransaction::PAYMENT_METHOD
             ? self::PAYMENT_METHOD_CREDIT_CARD_LABEL
             : self::PAYMENT_METHOD_BOLETO_LABEL;
+    }
+
+    /**
+     * @return Varien_Object
+     */
+    protected function _prepareSpecificInformation()
+    {
+        $specificInformation = [];
+
+        $transaction = $this->getTransaction();
+
+        if (!is_null($transaction)) {
+            $installments = $transaction->getInstallments();
+            if (is_null($installments)) {
+                $installments = 1;
+            }
+
+            $specificInformation = array_merge($specificInformation, [
+                'Payment Method' => $this->getPaymentMethod(),
+                'Installments' => $installments
+            ]);
+        }
+
+        return new Varien_Object($specificInformation);
     }
 }
