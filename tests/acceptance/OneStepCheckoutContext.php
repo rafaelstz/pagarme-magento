602752744d34001bcbce37f9011d9bfb8da27b27
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2017-05-03 15:04:42 -0300

    Finish purchase by boleto wihtout discount on OSC

--- tests/acceptance/OneStepCheckoutContext.php
+++ tests/acceptance/OneStepCheckoutContext.php
@@ -12,10 +12,13 @@ class OneStepCheckoutContext extends RawMinkContext
     use PagarMe\Magento\Test\Helper\CustomerDataProvider;
     use PagarMe\Magento\Test\Helper\PagarMeSettings;
     use PagarMe\Magento\Test\Helper\ProductDataProvider;
+    use PagarMe\Magento\Test\Helper\Interaction;
 
     protected $adminUser;
+    protected $pagarMeCheckout;
+    protected $product;
 
-    const INOVARTI_CHECKOUT_ON = 1;
+    const INOVARTI_CHECKOUT_ON  = 1;
     const INOVARTI_CHECKOUT_OFF = 0;
 
     /**
@@ -45,7 +48,50 @@ class OneStepCheckoutContext extends RawMinkContext
      */
     public function iConfirmPayment()
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $page = $this->getSession()->getPage();
+
+        $page->find('css', '#p_method_pagarme_checkout')->click();
+        $this->getSession()->wait(5000);
+
+        $button = $page->find('css', '#pagarme-checkout-fill-info-button');
+
+        $page->find('css', '#pagarme-checkout-fill-info-button')->click();
+        $this->getSession()->wait(5000);
+
+        $this->getSession()->switchToIframe(
+            $page->find('css', 'iframe')->getAttribute('name')
+        );
+
+        $this->pagarMeCheckout = $this->getSession()->getPage();
+        $this->pagarMeCheckout->pressButton('Boleto');
+
+        $this->waitForElement(
+            '#pagarme-modal-box-step-buyer-information',
+            1000
+        );
+
+        $this->pagarMeCheckout->find(
+            'css',
+            '#pagarme-modal-box-step-buyer-information .pagarme-modal-box-next-step'
+        )->click();
+
+        $this->waitForElement(
+            '#pagarme-modal-box-step-customer-address-information',
+            1000
+        );
+
+        $this->pagarMeCheckout->find(
+            'css',
+            '#pagarme-modal-box-step-customer-address-information .pagarme-modal-box-next-step'
+        )->click();
+
+        $page = $this->getSession()->wait(5000);
+
+        $this->getSession()->switchToIframe();
+        $page = $this->getSession()->getPage();
+        $page->pressButton(Mage::helper('pagarme_checkout')->__('Place Order'));
+
+        $this->getSession()->wait(5000);
     }
 
     /**
@@ -53,7 +99,24 @@ class OneStepCheckoutContext extends RawMinkContext
      */
     public function thePurchaseMustBeCreatedWithSuccess()
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $page = $this->getSession()->getPage();
+
+        $successMessage = $page->find('css', 'h1')
+            ->getText();
+
+        \PHPUnit_Framework_TestCase::assertEquals(
+            getenv('MAGENTO_URL') . 'index.php/checkout/onepage/success/',
+            $this->getSession()->getCurrentUrl()
+        );
+
+        \PHPUnit_Framework_TestCase::assertEquals(
+            strtolower(
+                Mage::helper(
+                    'pagarme_checkout'
+                )->__('Your order has been received.')
+            ),
+            strtolower($successMessage)
+        );
     }
 
     /**
@@ -61,7 +124,23 @@ class OneStepCheckoutContext extends RawMinkContext
      */
     public function aLinkToBoletoMustBeProvided()
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $page = $this->getSession()->getPage();
+
+        \PHPUnit_Framework_TestCase::assertContains(
+            'Para imprimir o boleto',
+            $page->find(
+                'css',
+                '.pagarme_info_boleto'
+            )->getText()
+        );
+
+        \PHPUnit_Framework_TestCase::assertContains(
+            'https://pagar.me',
+            $page ->find(
+                'css',
+                '.pagarme_info_boleto a'
+            )->getAttribute('href')
+        );
     }
 
     private function enableInovartiOneStepCheckout()
