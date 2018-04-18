935d217ce0d929986fc8c77ad26ee825c20b1e2b
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-04-18 15:08:26 -0300

    tests: parallelize e2e
    Co-authored-by: Murilo Souza <murilohns@gmail.com>

--- script/test-step2-e2e.sh
+++ script/test-step2-e2e.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose exec -T magento vendor/bin/behat --stop-on-failure
+docker-compose exec -T magento vendor/bin/behat -s configure --stop-on-failure
+docker-compose exec -T magento vendor/bin/behat -s $TEST_SUITE --stop-on-failure
