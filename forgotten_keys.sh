b77f70f5cef75e4a9cd010d6effe0af48e3ab8d2
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-07-03 21:06:47 -0300

    travis: create shell script to check hardcoded keys
    Creates a sh script to check if there's hardcoded keys on the code. It
    uses a grep command to check all files recursivelly using a regex.

---
+++ forgotten_keys.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+grep -REl "(ak_test|ak_live|ek_test|ek_live)_([a-zA-Z0-9]*)" .
+LINES_FOUNDED=$?
+if [ ! $LINES_FOUNDED -eq 1 ]; then
+    # key was founded so exit 1
+    exit 1
+fi
