7b8db1832fd3a9aced9f269d12e6704500bb2a92
Murilo Henrique <murilohns@gmail.com>
2019-02-08 16:06:36 -0200

    translations: add missing translations

--- app/code/community/PagarMe/Core/Model/System/Config/Source/BoletoDiscountMode.php
+++ app/code/community/PagarMe/Core/Model/System/Config/Source/BoletoDiscountMode.php
@@ -7,16 +7,23 @@ class PagarMe_Core_Model_System_Config_Source_BoletoDiscountMode
     const PERCENTAGE = 'percentage';
 
     /**
+     * @var PagarMe_Core_Helper_Data
+     */
+    protected $pagarmeModalHelper;
+
+    /**
      * @codeCoverageIgnore
      *
      * @return array
      */
     public function toOptionArray()
     {
+        $pagarmeModalHelper = Mage::helper('pagarme_modal');
+
         return [
-            self::NO_DISCOUNT => 'No discount',
-            self::FIXED_VALUE => 'Fixed value',
-            self::PERCENTAGE => 'Percentage'
+            self::NO_DISCOUNT => $pagarmeModalHelper->__('No discount'),
+            self::FIXED_VALUE => $pagarmeModalHelper->__('Fixed value'),
+            self::PERCENTAGE => $pagarmeModalHelper->__('Percentage')
         ];
     }
 }
